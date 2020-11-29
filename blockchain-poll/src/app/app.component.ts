30610bc7a748a8bcbd7212032aa1a09a4ef9b5f4
nal95 <alexnalji@gmail.com>
2020-11-29 06:55:43 +0100

    poll component logic

--- blockchain-poll/src/app/app.component.ts
+++ blockchain-poll/src/app/app.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   showForm = false;
+
+  polls = [
+    {
+      question: 'Do you like dogs or cats?',
+      image: 'https://images.pexels.com/photos/46024/pexels-photo-46024.jpeg?cs=srgb&dl=pexels-snapwire-46024.jpg&fm=jpg',
+      votes: [0,5,7],
+      voted: true,
+    },
+    {
+      question: 'Do you like dogs or cats?',
+      image: 'https://images.pexels.com/photos/406014/pexels-photo-406014.jpeg?auto=compress&cs=tinysrgb&h=650&w=940',
+      votes: [1,6,4],
+      voted: false,
+    },
+
+  ];
 }
